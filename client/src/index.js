d1652668824d031f63805740f920b24936ac57de
michelletaing <michelletaing@Michelles-MacBook-Air.local>
2022-06-29 12:05:01 -0400

    updated

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
+const client = new ApolloClient ({
+  uri: 'http://localhost:4000', // Specifies location of GraphQL server
+  cache: new InMemoryCache()
+});
+
+// ApolloProvider uses React's Context API to make a configured Apollo Client instance available throughout a React component tree
+// Allows all pages, containers, and components can access the client via friendly React Hooks thanks to the context API
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
